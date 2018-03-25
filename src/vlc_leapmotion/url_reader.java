a1ed3af74d1f9431a92635cdb84663fb2ac08183
Tushaar Gangarapu <tushaargvsg45@gmail.com>
2018-03-25 16:59:33 +0530

    Update url_reader.java

--- src/vlc_leapmotion/url_reader.java
+++ src/vlc_leapmotion/url_reader.java
@@ -2,34 +2,34 @@
  * This file reads content from web page. Modified to work with hue_json.java
  */
 
-package vlc_leapmotion;
+package vlc_leapmotion ;
 
-import java.net.*;
-import java.io.*;
+import java.net.* ;
+import java.io.* ;
 
 public class url_reader {
 	public static String address(String x) {
-		String nextLine;
-		URL url;
-		URLConnection urlConn;
-		InputStreamReader  inStream;
-		BufferedReader buff;
+		String nextLine ;
+		URL url ;
+		URLConnection urlConn ;
+		InputStreamReader  inStream ;
+		BufferedReader buff ;
 		
-		try{
-			url  = new URL(x) ;
+		try {
+			url = new URL(x) ;
        		urlConn = url.openConnection() ;
        		inStream = new InputStreamReader(urlConn.getInputStream()) ;
        		buff= new BufferedReader(inStream) ;
         
-            nextLine =buff.readLine();  
-            if (nextLine !=null)
-                return nextLine;
-            return null;
+            nextLine =buff.readLine() ;  
+            if (nextLine != null)
+                return nextLine ;
+            return null ;
         } catch(MalformedURLException e) {
-       		System.out.println("Please check the URL:" + e.toString()) ;
+       		System.out.println("Please check the URL: " + e.toString()) ;
 		} catch(IOException  ioe) {
-      		System.out.println("Can't read  from the Internet: "+ ioe.toString()); 
+      		System.out.println("Can't read  from the Internet: " + ioe.toString()) ; 
 		}
-       return null;
+       return null ;
 	}  
 }
